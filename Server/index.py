2944313e597929402c50b8ea5d9b5b5d00dc9da2
efeemir103 <efeemir103@gmail.com>
2019-01-05 19:35:12 +0300

    expanded player class

--- Server/index.py
+++ Server/index.py
@@ -1,7 +1,12 @@
 import socketio
 import mysql
 class Player:
-  def __init__(self,name,id,points):
+  def __init__(self,name,id,points,banned,admin,mod):
     self.name=name
     self.id=id
     self.points=points
+    self.banned=banned
+    self.admin=admin
+    self.mod=mod
+  
+    
