8129b7b24ee3f8c46f4b1d870643c73cfdbba133
nycrera <alperenyurdakul7@gmail.com>
2019-01-05 18:23:02 +0300

    as

--- pgame_mysql_create.sql
+++ pgame_mysql_create.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `users` (
 	`id` smallint NOT NULL AUTO_INCREMENT,
-	`username` TEXT NOT NULL UNIQUE,
-	`password` TEXT NOT NULL,
-	`points` int NOT NULL DEFAULT '0',
-	`banned` tinyint NOT NULL DEFAULT '0',
-	`admin` tinyint NOT NULL DEFAULT '0',
+	`username` VARCHAR(255) NOT NULL UNIQUE,
+	`password` VARCHAR(255) NOT NULL,
+	`points` int NOT NULL DEFAULT 0,
+	`banned` tinyint NOT NULL DEFAULT 0,
+	`admin` tinyint NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`)
 );
-
