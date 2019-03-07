d4816ed484802c64111ff43e0909fb7ecca24e81
Efe Emir Pekmez <efeemir103@gmail.com>
2019-03-07 22:11:31 +0300

    Update client.py

--- Client/src/client.py
+++ Client/src/client.py
@@ -1,5 +1,13 @@
 from gi.repository import Gtk
 class Game(Gtk.Window):
-  
+  Gtk.Window.___init___(self, title="P-Game")
+  self.become_server_button = Gtk.Button(label="Become a server")
+  self.connect_confirm_button = Gtk.Button(label="Connect to server")
+  self.become_server_button.connect("clicked", self.become_server)
+  self.connect_confirm_button.connect("clicked", self.connect_server)
+  def become_server(self):
+    #insert code to accept connections
+  def connect_server(self):
+    #insert code to connect to ip/domain entered in textfield.
 
 window = Game()
